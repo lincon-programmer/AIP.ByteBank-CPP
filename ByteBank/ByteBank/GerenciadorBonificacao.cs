f56cc1a9c7f24a8dc4a20f25cbde28c4cd63c9d6
lincon-programmer <lincongallo@gmail.com>
2021-11-02 18:24:13 -0300

    feat: Novas funcionalidades

---
+++ ByteBank/ByteBank/GerenciadorBonificacao.cs
@@ -0,0 +1,24 @@
+﻿using ByteBank.Funcionarios;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ByteBank
+{
+    class GerenciadorBonificacao
+    {
+        private double _TotalBonificacao;
+
+        public void Registrar(Funcionario funcionario)
+        {
+            _TotalBonificacao += funcionario.GetBonificacao();
+        }
+
+        public double GetTotalBonificacao()
+        {
+            return _TotalBonificacao;
+        }
+    }
+}
